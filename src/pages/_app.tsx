75f0a4ef1de6fd823ccf463f440c153b91c343f9
tnrdd <tnrdd@tutanota.com>
2022-04-13 09:41:42 +0000

    add reducer types

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,12 @@ import { AppProps } from "next/app";
 import Layout from "../components/layout";
 import reducer from "../reducers/cart";
 import "../styles/global.css";
-
+import {Product} from "../types/product";
 export const CartContext: any = createContext([]);
+const initialState: Product[] = []
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [cart, dispatch]: any = useReducer(reducer, []);
+  const [cart, dispatch] = useReducer(reducer, initialState);
 
   return (
     <CartContext.Provider value={{ cart, dispatch }}>
